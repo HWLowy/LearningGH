b7e0ba21f75a4a67fdf413e1c2a7e46893524f73
Bright Levi Bennett <bright.bennett@icloud.com>
2023-04-17 13:29:15 -1000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Bert Bennett on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
